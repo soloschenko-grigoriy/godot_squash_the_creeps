cd1255b964efe6b343f63694e095640edfa1ce9b
Greg Solo <gregory.soloshchenko@gmail.com>
2023-09-22 21:16:28 -0400

    character animation

--- mob.gd
+++ mob.gd
@@ -15,6 +15,8 @@ func initialize(start_position: Vector3, player_position: Vector3):
 	var random_speed = randi_range(min_speed, max_speed)
 	velocity = Vector3.FORWARD * random_speed
 	velocity = velocity.rotated(Vector3.UP, rotation.y)
+	
+	$AnimationPlayer.speed_scale = random_speed / min_speed
 
 func _on_visible_on_screen_notifier_3d_screen_exited():
 	queue_free()
--- player.gd
+++ player.gd
@@ -24,6 +24,9 @@ func _physics_process(delta):
 	if direction != Vector3.ZERO:
 		direction = direction.normalized()
 		$Pivot.look_at(position + direction, Vector3.UP)
+		$AnimationPlayer.speed_scale = 4
+	else:
+		$AnimationPlayer.speed_scale = 1
 		
 	target_velocity.x = direction.x * speed
 	target_velocity.z = direction.z * speed
@@ -48,6 +51,7 @@ func _physics_process(delta):
 				target_velocity.y = bounce_impulse
 				
 	move_and_slide()
+	$Pivot.rotation.x = PI / 6 * velocity.y / jump_impluse
 	
 func die():
 	hit.emit()
